b7d87961fe479f740fc2b2656f19a2895450278e
Amaranvijay <vijay936173@gmail.com>
2026-07-07 10:12:17 +0530

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -6,7 +6,7 @@
  */
 // const n= parselnt();
 const n = parselnt(process.argv[2]);
-function isPrime(num){
+function isPrime(num) ^{
     if (num <=1) return false;
 
     for(let i =2;i < num; i++) {
