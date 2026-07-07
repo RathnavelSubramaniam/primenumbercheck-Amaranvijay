e80836e748205314c6bfd82ffc67f5e2715c2ba4
Amaranvijay <vijay936173@gmail.com>
2026-07-07 10:21:17 +0530

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -6,7 +6,7 @@
  */
 // const n=parseInt();
 const n = parseInt(process.argv[2]);
-function isPrime(num)^{
+function isPrime(num) {
     if (num <=1) return false;
 
     for(let i =2;i < num; i++) {
@@ -18,9 +18,9 @@ function isPrime(num)^{
 }
 
 if(isPrime(n)) {
-    console.log("Prime number");
+    console.log("Prime Number");
 }else{
-    console.log("Not Prime number");
+    console.log("Not Prime Number");
 }
 
 module.exports = isPrime;
